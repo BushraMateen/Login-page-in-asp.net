960dbfbef43d2b0e075f05fec347fabe48e947ec
Bushra <Bushra@DESKTOP-M8KOD4K>
2024-01-18 20:03:14 +0530

    implemented login validation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,30 @@
 ﻿using login_page.Models;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace login_page.Controllers
 {
     public class HomeController : Controller
     {
+         private BushraDbEntities4 bushraDbEntities2 = new BushraDbEntities4();
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
+            if (Session["idUser"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
         }
 
         public ActionResult About()
@@ -61,6 +73,32 @@ namespace login_page.Controllers
         {
             return View();
         }
+        public ActionResult LogIn(string FirstName, string Password)
+        {
+            BushraDbEntities4 bushraDbEntities2 = new BushraDbEntities4();
+            //var v = bushraDbEntities2.Registers.FirstOrDefault(s => s.FirstName.Equals(FirstName) && s.Password.Equals(Password));
+
+            var v = bushraDbEntities2.Registers.FirstOrDefault(s => s.FirstName.Equals(FirstName) && s.Password.Equals(Password));
+
+
+            if (v != null)
+            {
+                Session["idUser"] = v.FirstName + " " + v.LastName;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.error = "login Failed";
+                return RedirectToAction("LoginPage");
+            }
+        
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Homepage");
+        }
+
     }
 }
     
